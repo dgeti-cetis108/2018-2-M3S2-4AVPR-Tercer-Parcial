f0f421317a2b0497521ba0fa2250a173d97de1e6
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2019-05-13 13:52:33 -0600

    POST Request
    Configuración de una petición post, para validar el inicio de sesión
    
    TODO: realizar la petición post con acceso a base de datos sqlite

--- 01-WebAPI/index.js
+++ 01-WebAPI/index.js
@@ -1,16 +1,34 @@
-var express = require('express')
-var app = express()
-var port = 3000
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var port = 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hola Dummies!')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', function (req, res) {
+//   res.send('Hola Dummies!')
+// })
 
 // mostrar archivos estaticos
 // http://localhost:3000/estaticos
-app.use('/estaticos', express.static('dist'))
+// app.use('/estaticos', express.static('dist'))
 
+// acceso para validar el inicio de sesion
+// http://localhost:3000/api/login
+app.post("/api/login", (req, res) => {
+  let user = req.body.user;
+  let password = req.body.password;
 
-app.listen(port, function () {
-  console.log(`Servidor ExpressJS corriendo en el puerto ${port}`)
-})
+  if (user == "bidkar" && password == "123") {
+    console.log("usuario y contraseña correctos");
+    res.send(true);
+  } else {
+    console.log("inicio de sesión inválido");
+    res.send(false);
+  }
+});
+
+app.listen(port, function() {
+  console.log(`Servidor ExpressJS corriendo en el puerto ${port}`);
+});
